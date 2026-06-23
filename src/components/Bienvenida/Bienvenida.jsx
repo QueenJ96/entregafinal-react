7f163db5c3100edc7f12148b84d59b855c378d01
Julia Martinez <juliaamartinezz@gmail.com>
2026-06-23 08:43:10 -0300

    cambios leves

--- src/components/Bienvenida/Bienvenida.jsx
+++ src/components/Bienvenida/Bienvenida.jsx
@@ -1,7 +1,7 @@
 export const Bienvenida = () => {
   return (
     <div style={{ padding: "2rem", textAlign: "center", color: "var(--color-secundario)" }}>
-      <h2>¡Bienvenida a nuestra tienda aesthetic! 🎀</h2>
+      <h2>¡Bienvenida a nuestra tienda! 🎀</h2>
     </div>
   );
 };
